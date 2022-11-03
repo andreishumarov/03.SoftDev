74c8e99973b7b0154a328865386ca091c4d06c32
andreishumarov <114391036+andreishumarov@users.noreply.github.com>
2022-11-03 13:05:40 +0200

    Add files via upload

---
+++ Реализиране на MVC/ConsoleMVC/Controllers/TipCalculatorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleMVC.Model;
+using ConsoleMVC.Views;
+
+namespace ConsoleMVC.Controllers
+{
+    class TipCalculatorController
+    {
+        private Tip tip;
+        private Display display;
+        public TipCalculatorController()
+        {
+            display = new Display();
+            tip = new Tip(display.Amount, display.Percent);
+            display.TipAmount = tip.CalculateTip();
+            display.ShowTipAndTotal();
+        }
+    }
+}
---
+++ Реализиране на MVC/ConsoleMVC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsoleMVC.Controllers;
+using ConsoleMVC.Model;
+using ConsoleMVC.Views;
+
+
+namespace ConsoleMVC
+{
+    class Program
+    {
+       static void Main(string[]args)
+        {
+            TipCalculatorController tipCalculatorController = new TipCalculatorController();
+        }
+        }
+    }
